17c6742f4176a8587924ec0c386d4a4851a95452
billyprasasti <43290087+billyprasasti@users.noreply.github.com>
2018-09-15 14:29:42 +0700

    master

---
+++ jscript.js
@@ -0,0 +1,27 @@
+$("#btn_hasil").click(function(){
+	var angka1 = parseInt($("#angka1").val());
+	var angka2 = parseInt($("#angka2").val());
+	var operator = $("#operator").val();
+
+	if (operator == "+") {
+		var hasil = angka1 + angka2;
+	}
+
+
+	if (operator == "-") {
+		var hasil = angka1 - angka2;
+	}
+
+
+	if (operator == "*") {
+		var hasil = angka1 * angka2;
+	}
+
+
+	if (operator == "/") {
+		var hasil = angka1 / angka2;
+	}
+
+
+	$("#hasil").val(hasil);
+});
